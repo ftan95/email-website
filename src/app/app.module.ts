172679f55b813fe444b47f774aef457c8a7d4773
Frederick <Frederick@pein.attlocal.net>
2019-06-29 09:42:32 -0700

    uploading files to git

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmailComponent } from './email/email.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmailComponent
   ],
   imports: [
     BrowserModule,
